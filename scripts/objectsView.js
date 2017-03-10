b9e195298cbf8011247aab6660d0ed1ddea91d2d
F Grayson Meindl <meindlgrayson@gmail.com>
2017-03-10 15:44:54 -0800

    tabbed navigation added. bug involving hamburger menu fixed.

--- scripts/objectsView.js
+++ scripts/objectsView.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
 
 $('.icon-align-justify').click(function() {
   $('.icon-align-justify').toggleClass('rotate');
+  // $('#menu').hide();
   $('.nav-one li').toggle('slow');
 });
 
@@ -17,4 +18,16 @@ objectsView.handleMainNav = function() {
   });
 };
 
+objectsView.handleNavDrop = function() {
+  $('.nav-drop').on('click', '.tab', function() {
+    console.log($('.nav-drop'));
+    $('.icon-align-justify').toggleClass('rotate');
+    $('.nav-one li').toggle('slow');
+    $('.tab-content').hide();
+    $('#articles').hide();
+    $('#' + $(this).data('content')).fadeIn();
+  });
+};
+
 objectsView.handleMainNav();
+objectsView.handleNavDrop();
